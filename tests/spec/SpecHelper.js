8ff5ca383c20d56c459e3f2692a75afc6c671b4a
Sean Noble <sean.noble.72@gmail.com>
2013-09-09 17:49:02 +0200

    Add test helper to check an array has all values within a range

--- tests/spec/SpecHelper.js
+++ tests/spec/SpecHelper.js
@@ -1,1 +1,16 @@
+beforeEach(function() {
+  this.addMatchers({
+    toBeLessThan: function(expected) {
+      return this.actual < expected;
+    },
+    toHaveAllValuesInRange: function (low, high) {
+        for (var idx = 0; idx < this.actual.length; idx += 1) {
+            if (this.actual[idx] < low || this.actual[idx] > high) {
+                return false;
+            }
+        }
+        return true;
+    }
+  });
+});
 
